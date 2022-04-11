510719b9f3b5fccc7712fe3a0a4d30dceb9be333
iwin <418577121@qq.com>
2022-04-11 17:21:47 +0800

    Initial commit

---
+++ src/com/dht/java/lambda/LamdbaTest.java
@@ -0,0 +1,12 @@
+package com.dht.java.lambda;
+
+/**
+ * @project_name: java17
+ * @package_name: com.dht.java.lambda
+ * @description:
+ * @author: DingHaiTing
+ * @create_time: 2022-04-11 17:19
+ **/
+
+public class LamdbaTest {
+}
