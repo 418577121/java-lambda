c7ac19b91add86123f4b1c0437114dd633c1bc99
iwin <418577121@qq.com>
2022-04-12 14:14:59 +0800

    commit!!!

--- src/com/dht/java/lambda/stream/StreamTest.java
+++ src/com/dht/java/lambda/stream/StreamTest.java
@@ -1,7 +1,13 @@
 package com.dht.java.lambda.stream;
 
+import com.dht.java.lambda.bean.Student;
+
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -14,6 +20,12 @@ import java.util.stream.Stream;
 
 public class StreamTest {
     public static void main(String[] args) {
-
+      ArrayList<Student> list = new ArrayList();
+      for (int i = 0; i < 100; i++) {
+        list.add(new Student("测试" + i, i));
+      }
+      // filter方法
+      List<Student> collect = list.stream().filter(student -> student.getAge() > 98).collect(Collectors.toList());
+      System.out.println(collect);
     }
 }
